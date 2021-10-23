7b61b6602e6dd9f20fc5d7a1a555197e79f0e31d
Jeriel Benavides <jeriel.benavides@dcmail.ca>
2021-10-22 23:02:40 -0400

    License Attributions

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 Swal.fire({
     title: '<strong class="titleFont">MEET WATS</strong>',
     html:
-    '<p>Warfare Assistant Type 9S</p>',
+    '<p class="popup_text">Warfare Assistant Type 9S</p>' + 
+    '<p class="popup_text">(He is totally friendly now, ahem, sometimes)</p>' + 
+    '<br>' + 
     '<img id="wat9s_popup_img" style="height:30vh" src="/img/popup.svg"></img>',
     showCloseButton: true,
     confirmButtonText: "LET'S PLAY"
-  });
+});
 
 window.onload = function(){
     //Audio
@@ -69,6 +71,27 @@ window.onload = function(){
 
     let buttons = [neutralBtn, happyBtn, deadBtn, angryBtn, sadBtn, sickBtn];
 
+    let audioAuthors = document.getElementById('footer_link_1');
+    audioAuthors.addEventListener('click', function(){
+        Swal.fire({
+            title: '<strong class="white_text">Authors</strong>',
+            html:
+            '<div id="authors_div"' + 
+            '<p class="popup_text">Chainsaw: <a target="_blank" href="https://freesound.org/people/domiscz/sounds/461734/">FreeSound.org</a></p>' + 
+            '<p class="popup_text">Cheerful: <a target="_blank" href="https://freesound.org/people/Setuniman/sounds/171394/">FreeSound.org</a></p>' + 
+            '<p class="popup_text">Droplet: <a target="_blank" href="https://freesound.org/people/gkillhour/sounds/267221///">FreeSound.org</a></p>' + 
+            '<p class="popup_text">Electric Current: <a target="_blank" href="https://freesound.org/people/FlashTrauma/sounds/398274/">FreeSound.org</a></p>' + 
+            '<p class="popup_text">Heartbeat: <a target="_blank" href="https://freesound.org/people/andres_marcos/sounds/74701/">FreeSound.org</a></p>' + 
+            '<p class="popup_text">Metals falling: <a target="_blank" href="https://freesound.org/people/manimato2/sounds/429998/">FreeSound.org</a></p>' + 
+            '<p class="popup_text">Metal impact: <a target="_blank" href="https://freesound.org/people/Rickplayer/sounds/530486/">FreeSound.org</a></p>' + 
+            '<p class="popup_text">Metal hit: <a target="_blank" href="https://freesound.org/people/Rickplayer/sounds/530486/">FreeSound.org</a></p>' + 
+            '</div>' +
+            '<br>' + 
+            '<img id="wat9s_popup_img" style="height:20vh" src="/img/popup.svg"></img>',
+            showCloseButton: true,
+            confirmButtonText: "BACK TO PLAYING"
+        });
+    });
     // // ! EXAMPLE OF TARGETTING MULTIPLE SELECTORS NOTE THAT WE CAN STORE
     // // ! VARIABLES INTO AN ARRAY AND TARGET THE ARRAY IN GSAP3!!!
     let pupils = [leftPupil, rightPupil];
